91e7ba311377ee3a1b892f01433fe1011c57b08c
Gabriel Rosser <gabriel@iwoca.co.uk>
2014-05-25 09:18:50 +0100

    Modified settings for use with gunicorn.  Added static file path.  TODO: commit EMPTY static dir within geodjango directory

--- geodjango/geodjango/settings.py
+++ geodjango/geodjango/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = (
     'django.contrib.gis',
     'south',
     'traveltime',
+    'gunicorn',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -74,5 +75,47 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
-
+STATIC_ROOT = 'static'
 STATIC_URL = '/static/'
+
+# logging config
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console':{
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'mail_admins': {
+            'level': 'ERROR',
+            'class': 'django.utils.log.AdminEmailHandler',
+            'filters': [],
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['null'],
+            'propagate': True,
+            'level': 'INFO',
+        },
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    }
+}
--- geodjango/geodjango/urls.py
+++ geodjango/geodjango/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf.urls.static import static
 from django.contrib import admin
+import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -10,3 +11,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
